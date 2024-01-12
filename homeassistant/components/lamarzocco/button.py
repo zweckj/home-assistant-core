a207ba3aac73f1c566274eb22804dd4869d77f6f
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-12 13:48:42 +0100

    add button

---
+++ homeassistant/components/lamarzocco/button.py
@@ -0,0 +1,59 @@
+"""Button platform for La Marzocco espresso machines."""
+
+from collections.abc import Callable, Coroutine
+from dataclasses import dataclass
+from typing import Any
+
+from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .coordinator import LaMarzoccoUpdateCoordinator
+from .entity import LaMarzoccoEntity, LaMarzoccoEntityDescription
+
+
+@dataclass(frozen=True, kw_only=True)
+class LaMarzoccoButtonEntityDescription(
+    LaMarzoccoEntityDescription,
+    ButtonEntityDescription,
+):
+    """Description of an La Marzocco Button."""
+
+    press_fn: Callable[[LaMarzoccoUpdateCoordinator], Coroutine[Any, Any, None]]
+
+
+ENTITIES: tuple[LaMarzoccoButtonEntityDescription, ...] = (
+    LaMarzoccoButtonEntityDescription(
+        key="start_backflush",
+        translation_key="start_backflush",
+        icon="mdi:water-sync",
+        press_fn=lambda coordinator: coordinator.lm.start_backflush(),
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up button entities."""
+
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    async_add_entities(
+        LaMarzoccoButtonEntity(coordinator, description)
+        for description in ENTITIES
+        if coordinator.lm.model_name in description.supported_models
+    )
+
+
+class LaMarzoccoButtonEntity(LaMarzoccoEntity, ButtonEntity):
+    """Button supporting backflush."""
+
+    entity_description: LaMarzoccoButtonEntityDescription
+
+    async def async_press(self) -> None:
+        """Press button."""
+        await self.entity_description.press_fn(self.coordinator)
