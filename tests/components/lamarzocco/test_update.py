d2478df2a47a511977b76af8ced3fe5a9ab725fb
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-14 10:33:54 +0100

    simplify tests

--- tests/components/lamarzocco/test_update.py
+++ tests/components/lamarzocco/test_update.py
@@ -12,39 +12,21 @@ from homeassistant.helpers import entity_registry as er
 pytestmark = pytest.mark.usefixtures("init_integration")
 
 
-async def test_machine_firmware(
+async def test_update_entites(
     hass: HomeAssistant,
     mock_lamarzocco: MagicMock,
     entity_registry: er.EntityRegistry,
     snapshot: SnapshotAssertion,
 ) -> None:
-    """Test the La Marzocco Machine Firmware."""
+    """Test the La Marzocco update entities."""
 
     serial_number = mock_lamarzocco.serial_number
 
-    state = hass.states.get(f"update.{serial_number}_machine_firmware")
-    assert state
-    assert state == snapshot
+    for entity_name in ["machine_firmware", "gateway_firmware"]:
+        state = hass.states.get(f"update.{serial_number}_{entity_name}")
+        assert state
+        assert state == snapshot(name=f"{entity_name}-state")
 
-    entry = entity_registry.async_get(state.entity_id)
-    assert entry
-    assert entry == snapshot
-
-
-async def test_gateway_firmware(
-    hass: HomeAssistant,
-    mock_lamarzocco: MagicMock,
-    entity_registry: er.EntityRegistry,
-    snapshot: SnapshotAssertion,
-) -> None:
-    """Test the La Marzocco Gateway Firmware."""
-
-    serial_number = mock_lamarzocco.serial_number
-
-    state = hass.states.get(f"update.{serial_number}_gateway_firmware")
-    assert state
-    assert state == snapshot
-
-    entry = entity_registry.async_get(state.entity_id)
-    assert entry
-    assert entry == snapshot
+        entry = entity_registry.async_get(state.entity_id)
+        assert entry
+        assert entry == snapshot(name=f"{entity_name}-entry")
