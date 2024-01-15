a94041ff86c7b5f74cea03700b5a7720a3ffea09
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-15 03:04:52 +0100

    add install

--- homeassistant/components/lamarzocco/update.py
+++ homeassistant/components/lamarzocco/update.py
@@ -1,14 +1,17 @@
 """Support for La Marzocco Switches."""
 
-from collections.abc import Callable
+from collections.abc import Callable, Coroutine
 from dataclasses import dataclass
+from typing import Any
 
 from lmcloud import LMCloud as LaMarzoccoClient
+from lmcloud.const import LaMarzoccoUpdateableComponent
 
 from homeassistant.components.update import (
     UpdateDeviceClass,
     UpdateEntity,
     UpdateEntityDescription,
+    UpdateEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import EntityCategory
@@ -16,6 +19,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
+from .coordinator import LaMarzoccoUpdateCoordinator
 from .entity import LaMarzoccoEntity, LaMarzoccoEntityDescription
 
 
@@ -28,6 +32,7 @@ class LaMarzoccoUpdateEntityDescription(
 
     current_fw_fn: Callable[[LaMarzoccoClient], str]
     latest_fw_fn: Callable[[LaMarzoccoClient], str]
+    update_fn: Callable[[LaMarzoccoClient], Coroutine[Any, Any, bool]]
 
 
 ENTITIES: tuple[LaMarzoccoUpdateEntityDescription, ...] = (
@@ -38,6 +43,7 @@ ENTITIES: tuple[LaMarzoccoUpdateEntityDescription, ...] = (
         icon="mdi:cloud-download",
         current_fw_fn=lambda lm: lm.firmware_version,
         latest_fw_fn=lambda lm: lm.latest_firmware_version,
+        update_fn=lambda lm: lm.update_firmware(LaMarzoccoUpdateableComponent.MACHINE),
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     LaMarzoccoUpdateEntityDescription(
@@ -47,6 +53,7 @@ ENTITIES: tuple[LaMarzoccoUpdateEntityDescription, ...] = (
         icon="mdi:cloud-download",
         current_fw_fn=lambda lm: lm.gateway_version,
         latest_fw_fn=lambda lm: lm.latest_gateway_version,
+        update_fn=lambda lm: lm.update_firmware(LaMarzoccoUpdateableComponent.GATEWAY),
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
 )
@@ -72,6 +79,20 @@ class LaMarzoccoUpdateEntity(LaMarzoccoEntity, UpdateEntity):
 
     entity_description: LaMarzoccoUpdateEntityDescription
 
+    def __init__(
+        self,
+        coordinator: LaMarzoccoUpdateCoordinator,
+        description: LaMarzoccoUpdateEntityDescription,
+    ) -> None:
+        """Initialize the entity."""
+        super().__init__(coordinator, description)
+        self._update_in_progress = False
+
+    @property
+    def supported_features(self) -> UpdateEntityFeature:
+        """Flag supported features."""
+        return UpdateEntityFeature.INSTALL
+
     @property
     def installed_version(self) -> str | None:
         """Return the current firmware version."""
@@ -81,3 +102,13 @@ class LaMarzoccoUpdateEntity(LaMarzoccoEntity, UpdateEntity):
     def latest_version(self) -> str:
         """Return the latest firmware version."""
         return self.entity_description.latest_fw_fn(self.coordinator.lm)
+
+    async def async_install(
+        self, version: str | None, backup: bool, **kwargs: Any
+    ) -> None:
+        """Install an update."""
+        self._update_in_progress = True
+        self.async_write_ha_state()
+        await self.entity_description.update_fn(self.coordinator.lm)
+        self._update_in_progress = False
+        self.async_write_ha_state()
