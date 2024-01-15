9ff6d5e385728dea02ae4b9d3308072f07e8654f
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-15 04:07:26 +0100

    refine

--- homeassistant/components/lamarzocco/update.py
+++ homeassistant/components/lamarzocco/update.py
@@ -1,6 +1,6 @@
 """Support for La Marzocco Switches."""
 
-from collections.abc import Callable, Coroutine
+from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any
 
@@ -33,7 +33,7 @@ class LaMarzoccoUpdateEntityDescription(
 
     current_fw_fn: Callable[[LaMarzoccoClient], str]
     latest_fw_fn: Callable[[LaMarzoccoClient], str]
-    update_fn: Callable[[LaMarzoccoClient], Coroutine[Any, Any, bool]]
+    component: LaMarzoccoUpdateableComponent
 
 
 ENTITIES: tuple[LaMarzoccoUpdateEntityDescription, ...] = (
@@ -44,7 +44,7 @@ ENTITIES: tuple[LaMarzoccoUpdateEntityDescription, ...] = (
         icon="mdi:cloud-download",
         current_fw_fn=lambda lm: lm.firmware_version,
         latest_fw_fn=lambda lm: lm.latest_firmware_version,
-        update_fn=lambda lm: lm.update_firmware(LaMarzoccoUpdateableComponent.MACHINE),
+        component=LaMarzoccoUpdateableComponent.MACHINE,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     LaMarzoccoUpdateEntityDescription(
@@ -54,7 +54,7 @@ ENTITIES: tuple[LaMarzoccoUpdateEntityDescription, ...] = (
         icon="mdi:cloud-download",
         current_fw_fn=lambda lm: lm.gateway_version,
         latest_fw_fn=lambda lm: lm.latest_gateway_version,
-        update_fn=lambda lm: lm.update_firmware(LaMarzoccoUpdateableComponent.GATEWAY),
+        component=LaMarzoccoUpdateableComponent.GATEWAY,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
 )
@@ -115,7 +115,9 @@ class LaMarzoccoUpdateEntity(LaMarzoccoEntity, UpdateEntity):
         """Install an update."""
         self._update_in_progress = True
         self.async_write_ha_state()
-        success = await self.entity_description.update_fn(self.coordinator.lm)
+        success = await self.coordinator.lm.update_firmware(
+            self.entity_description.component
+        )
         if not success:
             raise HomeAssistantError("Update failed")
         self._update_in_progress = False
